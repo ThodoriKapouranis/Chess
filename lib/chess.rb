bbc9c5dc07b913559420762fa231c7ff36536437
ThodoriKapouranis <thodorikap@gmail.com>
2020-07-12 16:36:55 -0400

    add pawn movement, WITHOUT EN PASSANT

--- lib/chess.rb
+++ lib/chess.rb
@@ -46,38 +46,49 @@ class Chess
     to_y   = parse_tile(to)[0].join.to_i
     to_x   = parse_tile(to)[1].join.to_i
 
-    allowable_moves = @board[from_y][from_x].check_moves
-    if true   # allowable_moves.includes?([to_y,to_x])
-      @board[to_y][to_x]= @board[from_y][from_x]
-      @board[to_y][to_x].update_pos(to)
-      @board[from_y][from_x]="☐"
+    if @board[from_y][from_x]=="☐"
+      p "invalid starting tile"
+      return false
+    else
+      allowable_moves = @board[from_y][from_x].check_moves(@board)
     end
+
+    if allowable_moves.include?([to_y,to_x])
+      @board[to_y][to_x]= @board[from_y][from_x]
+      @board[to_y][to_x].update_pos(to_y,to_x)
+      @board[from_y][from_x]="☐"
+      return [to_y,to_x]
+    else
+      return false
+    end
+
+    
   end
   
-  private
   def default_board_start
-    @board[0][0]=Rook.new("black","a8")
-    @board[0][1]=Knight.new("black","b8")
-    @board[0][2]=Bishop.new("black","c8")
-    @board[0][3]=Queen.new("black","d8")
-    @board[0][4]=King.new("black","e8")
-    @board[0][5]=Bishop.new("black","f8")
-    @board[0][6]=Knight.new("black","g8")
-    @board[0][7]=Rook.new("black","h8")
-    8.times {|x| @board[1][x]=Pawn.new("black",@parse_hash[x]+"7")}
+    @board[0][0]=Rook.new("black",[0,0])
+    @board[0][1]=Knight.new("black",[0,1])
+    @board[0][2]=Bishop.new("black",[0,2])
+    @board[0][3]=Queen.new("black",[0,3])
+    @board[0][4]=King.new("black",[0,4])
+    @board[0][5]=Bishop.new("black",[0,5])
+    @board[0][6]=Knight.new("black",[0,6])
+    @board[0][7]=Rook.new("black",[0,7])
+    8.times {|x| @board[1][x]=Pawn.new("black",[1,x])}
 
-    @board[7][0]=Rook.new("white","a1")
-    @board[7][1]=Knight.new("white","b1")
-    @board[7][2]=Bishop.new("white","c1")
-    @board[7][3]=Queen.new("white","d1")
-    @board[7][4]=King.new("white","e1")
-    @board[7][5]=Bishop.new("white","f1")
-    @board[7][6]=Knight.new("white","g1")
-    @board[7][7]=Rook.new("white","h1")
-    8.times {|x| @board[6][x]=Pawn.new("white",@parse_hash[x]+"2")}
+    @board[7][0]=Rook.new("white",[7,0])
+    @board[7][1]=Knight.new("white",[7,1])
+    @board[7][2]=Bishop.new("white",[7,2])
+    @board[7][3]=Queen.new("white",[7,3])
+    @board[7][4]=King.new("white",[7,4])
+    @board[7][5]=Bishop.new("white",[7,5])
+    @board[7][6]=Knight.new("white",[7,6])
+    @board[7][7]=Rook.new("white",[7,7])
+    8.times {|x| @board[6][x]=Pawn.new("white",[6,x])}
 
   end
 
+  private
   def valid_tile?(tile)
     return false unless tile.length==2
     return false unless [tile[0]].any? {|x| ["a","b","c","d","e","f","g","h"].include?(x)}
@@ -101,12 +112,23 @@ class Piece
   end
 
   public
-  def update_pos(new_pos)
-    @position=new_pos
+  def update_pos(y_pos,x_pos)
+    @position=[y_pos,x_pos]
   end
+
   def to_s
     return @symbol
   end
+
+  private
+  def is_bound?(num) #bound within the tiles of the game
+    if (num>-1 and num<8)
+      return true
+    else
+      return false
+    end
+  end
+
 end
 
 class Pawn < Piece
@@ -115,11 +137,48 @@ class Pawn < Piece
     super(color,position)
     @symbol="♙" if @color.downcase=="white"
     @symbol="♟︎" if @color.downcase=="black"
+    @direction="up" if @color.downcase=="white"
+    @direction="down" if @color.downcase=="black"
   end
-  def check_moves
-  
+
+  def check_moves(board)
+    valid_moves=[]
+    cur_y=@position[0]
+    cur_x=@position[1]
+
+    if ( @direction=="up" && cur_y!=0)
+
+      unless (board[cur_y-1][cur_x]!="☐")   #Move forward
+        valid_moves.push([cur_y-1,cur_x]) 
+      end
+
+      if (is_bound?(cur_x-1) && board[cur_y-1][cur_x-1]!="☐") #NW
+        valid_moves.push([cur_y-1,cur_x-1])
+      end
+
+      if (is_bound?(cur_x+1) && board[cur_y-1][cur_x+1]!="☐") #NE
+        valid_moves.push([cur_y-1,cur_x+1])
+      end
+    end
+
+    if @direction=="down"
+      unless (cur_y==7 or board[cur_y+1][cur_x]!="☐")
+        valid_moves.push([cur_y+1,cur_x])
+      end
+
+      if (is_bound?(cur_x-1) && board[cur_y+1][cur_x-1]!="☐") #SW
+        valid_moves.push([cur_y+1,cur_x-1])
+      end
+
+      if (is_bound?(cur_x+1) && board[cur_y+1][cur_x+1]!="☐") #SE
+        valid_moves.push([cur_y+1,cur_x+1])
+      end
+
+    end
+    valid_moves
   end
 end
+
 class Knight < Piece
   attr_reader :symbol
   def initialize(color, position)
@@ -131,6 +190,7 @@ class Knight < Piece
   
   end
 end
+
 class Bishop < Piece
   attr_reader :symbol
   def initialize(color, position)
@@ -142,6 +202,7 @@ class Bishop < Piece
   
   end
 end
+
 class Rook < Piece
   attr_reader :symbol
   def initialize(color, position)
@@ -153,6 +214,7 @@ class Rook < Piece
   
   end
 end
+
 class Queen < Piece
   attr_reader :symbol
   def initialize(color, position)
@@ -164,6 +226,7 @@ class Queen < Piece
   
   end
 end
+
 class King < Piece
   attr_reader :symbol
   def initialize(color, position)
@@ -177,4 +240,6 @@ class King < Piece
 end
 
 game = Chess.new
-game.play()
+game.default_board_start
+game.make_move("a2","a3")
+game.make_move("a2","a3")
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -10,6 +10,71 @@ describe Piece do
       pawn = Pawn.new("black","a1")
       expect(pawn.symbol).to eql("♟︎")
     end
-    
+  end
+end
+
+describe Pawn do
+  describe '#check_moves'do
+    it "Does not go past pieces (white)" do
+      game = Chess.new
+      game.default_board_start
+      game.make_move("a2","a3")
+      game.make_move("a3","a4")
+      game.make_move("a4","a5")
+      game.make_move("a5","a6")
+      game.display
+      expect(game.make_move("a6","a7")).to eql(false)
+    end
+    it "Does not go past pieces (black)" do
+      game = Chess.new
+      game.default_board_start
+      game.make_move("a7","a6")
+      game.make_move("a6","a5")
+      game.make_move("a5","a4")
+      game.make_move("a4","a3")
+      game.display
+      expect(game.make_move("a3","a2")).to eql(false)
+    end
+    it "Can take NE" do
+      game = Chess.new
+      game.default_board_start
+      game.make_move("a2","a3")
+      game.make_move("a3","a4")
+      game.make_move("a4","a5")
+      game.make_move("a5","a6")
+      expect(game.make_move("a6","b7")).to eql([1,1])
+      game.display
+    end
+    it "Can take NW" do
+      game = Chess.new
+      game.default_board_start
+      game.make_move("h2","h3")
+      game.make_move("h3","h4")
+      game.make_move("h4","h5")
+      game.make_move("h5","h6")
+      expect(game.make_move("h6","g7")).to eql([1,6])
+      game.display
+    end
+    it "Can take SW" do
+      game = Chess.new
+      game.default_board_start
+      game.make_move("h7","h6")
+      game.make_move("h6","h5")
+      game.make_move("h5","h4")
+      game.make_move("h4","h3")
+      expect(game.make_move("h3","g2")).to eql([6,6])
+      game.display
+    end
+    it "Can take SE" do
+      game = Chess.new
+      game.default_board_start
+      game.make_move("a7","a6")
+      game.make_move("a6","a5")
+      game.make_move("a5","a4")
+      game.make_move("a4","a3")
+      expect(game.make_move("a3","b2")).to eql([6,1])
+      game.display
+    end
+
   end
 end
